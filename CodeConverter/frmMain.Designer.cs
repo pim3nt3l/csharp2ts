651b47b1bed14583260df9a40eafcf1e7c227be4
Yunior Pimentel <ypimentel@truenorth.pr>
2019-12-27 14:42:45 -0400

    initial version

---
+++ CodeConverter/frmMain.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace CodeConverter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.chkAllDirectory = new System.Windows.Forms.CheckBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.itemsListBox = new System.Windows.Forms.CheckedListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtPath);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(15, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(770, 41);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPath.Location = new System.Drawing.Point(90, 0);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(606, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button1.Location = new System.Drawing.Point(696, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Directory:";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.chkAllDirectory);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 21);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(770, 20);
+            this.panel3.TabIndex = 0;
+            // 
+            // chkAllDirectory
+            // 
+            this.chkAllDirectory.AutoSize = true;
+            this.chkAllDirectory.Checked = true;
+            this.chkAllDirectory.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAllDirectory.Dock = System.Windows.Forms.DockStyle.Left;
+            this.chkAllDirectory.Location = new System.Drawing.Point(0, 0);
+            this.chkAllDirectory.Name = "chkAllDirectory";
+            this.chkAllDirectory.Size = new System.Drawing.Size(90, 20);
+            this.chkAllDirectory.TabIndex = 3;
+            this.chkAllDirectory.Text = "All Directories";
+            this.chkAllDirectory.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.groupBox2);
+            this.panel4.Controls.Add(this.groupBox1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(15, 76);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(770, 359);
+            this.panel4.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtLog);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(410, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(360, 359);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Log Info";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Location = new System.Drawing.Point(3, 16);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(354, 340);
+            this.txtLog.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.itemsListBox);
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(10);
+            this.groupBox1.Size = new System.Drawing.Size(410, 359);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Items to Convert";
+            // 
+            // itemsListBox
+            // 
+            this.itemsListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.itemsListBox.FormattingEnabled = true;
+            this.itemsListBox.Location = new System.Drawing.Point(10, 23);
+            this.itemsListBox.Name = "itemsListBox";
+            this.itemsListBox.Size = new System.Drawing.Size(390, 291);
+            this.itemsListBox.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.checkBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(10, 314);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(390, 35);
+            this.panel2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.ForestGreen;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(292, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 35);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Convert";
+            this.button2.UseCompatibleTextRendering = true;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.checkBox2.Location = new System.Drawing.Point(0, 0);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(69, 35);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Select all";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel1);
+            this.Name = "frmMain";
+            this.Padding = new System.Windows.Forms.Padding(15, 35, 15, 15);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "C# to TypeScript Converter";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.CheckBox chkAllDirectory;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckedListBox itemsListBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CodeConverter/frmMain.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using RoslynTypeScript.Translation;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using TypescriptSyntaxPaste;
+using TypescriptSyntaxPaste.VSIX;
+
+namespace CodeConverter
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ofd = new FolderBrowserDialog();
+            ofd.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                this.txtPath.Text = ofd.SelectedPath;
+                this.GetFilesFromDirectory();
+            }
+        }
+
+        private void GetFilesFromDirectory()
+        {
+            var files = Directory.GetFiles(this.txtPath.Text, "*.cs", this.chkAllDirectory.Checked ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+            this.itemsListBox.Items.AddRange(files);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.itemsListBox.Items.Count > 0)
+                for (int i = 0; i < this.itemsListBox.Items.Count; i++)
+                {
+                    this.itemsListBox.SetItemChecked(i, this.checkBox2.Checked);
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.ConvertFiles(this.itemsListBox.CheckedItems.Cast<string>());
+        }
+
+        private void ConvertFiles(IEnumerable<string> items)
+        {
+            foreach (var item in items)
+            {
+                var text = File.ReadAllText(item);
+                var extracted = this.ConvertToTypescript(text, new ConvertSettings() { IsConvertToInterface = true, IsConvertMemberToCamelCase = true , IsConvertListToArray = true });
+                File.WriteAllText(item.Replace(".cs", ".ts"), extracted);
+                this.txtLog.AppendText($"Item: '{item}', exported\n");
+            }
+        }
+
+        public string ConvertToTypescript(string text, ConvertSettings settingStore)
+        {
+            try
+            {
+                CSharpSyntaxTree text1 = (CSharpSyntaxTree)CSharpSyntaxTree.ParseText(text, null, "", null, new CancellationToken());
+                if (text1.GetDiagnostics(new CancellationToken()).Any(f => f.Severity == (DiagnosticSeverity)3))
+                    return null;
+                CSharpSyntaxNode syntaxNode = text1.GetRoot(new CancellationToken());
+                if (this.IsEmptyRoot(syntaxNode))
+                    return null;
+
+                if (settingStore.IsConvertToInterface)
+                    syntaxNode = ClassToInterfaceReplacement.ReplaceClass(syntaxNode);
+
+                if (settingStore.IsConvertMemberToCamelCase)
+                    syntaxNode = MakeMemberCamelCase.Make(syntaxNode);
+
+                if (settingStore.IsConvertListToArray)
+                    syntaxNode = ListToArrayReplacement.ReplaceList(syntaxNode);
+
+                if (settingStore.ReplacedTypeNameArray.Length != 0)
+                    syntaxNode = TypeNameReplacement.Replace(settingStore.ReplacedTypeNameArray, syntaxNode);
+
+                if (settingStore.AddIPrefixInterfaceDeclaration)
+                    syntaxNode = AddIPrefixInterfaceDeclaration.AddIPrefix(syntaxNode);
+
+                if (settingStore.IsInterfaceOptionalProperties)
+                    syntaxNode = OptionalInterfaceProperties.AddOptional(syntaxNode);
+
+                CSharpSyntaxTree syntaxTree = (CSharpSyntaxTree)syntaxNode.SyntaxTree;
+                SyntaxTranslation syntaxTranslation = TF.Get(syntaxNode, null);
+                CSharpCompilation csharpCompilation = CSharpCompilation.Create("TemporaryCompilation", new[] { syntaxTree });
+                SemanticModel semanticModel = ((Compilation)csharpCompilation).GetSemanticModel(syntaxTree, false);
+                syntaxTranslation.Compilation = csharpCompilation;
+                syntaxTranslation.SemanticModel = semanticModel;
+                syntaxTranslation.ApplyPatch();
+                return syntaxTranslation.Translate();
+            }
+            catch (Exception ex)
+            {
+            }
+            return (string)null;
+        }
+
+        private bool IsEmptyRoot(SyntaxNode root)
+        {
+            return !root.DescendantNodes(null, false).Any();
+        }
+    }
+
+
+    public class ConvertSettings
+    {
+        public ConvertSettings()
+        {
+            this.ReplacedTypeNameArray = new TypeNameReplacementData[0];
+        }
+        public bool IsConvertMemberToCamelCase { get; set; }
+        public bool IsConvertToInterface { get; set; }
+        public bool IsConvertListToArray { get; set; }
+        public bool IsInterfaceOptionalProperties { get; set; }
+        public TypeNameReplacementData[] ReplacedTypeNameArray { get; set; }
+        public bool AddIPrefixInterfaceDeclaration { get; set; }
+    }
+}
